1443d2378aff27ec2f525c604e4d8e1e60eec82b
Catherine_AMF <catherineamf@gmail.com>
2021-12-30 17:32:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const questions = [
+    {
+        question: 'Сколько будет 2+2?',
+        type: 'prompt',
+        answer: 4
+    },
+    {
+        question: 'Солнце встает на востоке?',
+        type: 'confirm',
+        answer: true
+    },
+    {
+        question: 'Сколько будет 5 / 0?',
+        type: 'prompt',
+        answer: 'NaN'
+    },
+    {
+        question: 'Какого цвета небо?',
+        type: 'prompt',
+        answer: 'Голубое'
+    },
+    {
+        question: 'Главный вопрос жизни, вселенной и всего такого',
+        type: 'prompt',
+        answer: 42
+    }
+];
+
+const scores = startQuiz(questions);
+
+alert(scores);
+
+function startQuiz(questions) {
+    let points = 0;
+    let enteredAnswer;
+    for (let i = 0; i < questions.length; i++) {
+        let question = questions[i].question;
+
+        if (questions[i].type == 'confirm') {
+            enteredAnswer = confirm(question);
+        }
+        if (questions[i].type == 'prompt') {
+            enteredAnswer = prompt(question);
+        }
+
+        let correctAnswer = questions[i].answer;
+
+        if (correctAnswer == enteredAnswer) {
+            points += 10;
+        }
+
+    }
+    return points;
+}
